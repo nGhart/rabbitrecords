b514e252eaf21641f61a094da066506b88bd5a9d
nGhart <107068059+nGhart@users.noreply.github.com>
2023-10-17 16:55:53 +0000

    Update index.js url

--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ import { persistor, store } from './store/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 
-//axios.defaults.baseURL = 'https://server2-763m.onrender.com';
-axios.defaults.baseURL = 'http://localhost:1995';
-//axios.defaults.baseURL = 'https://test-pwvq.onrender.com/';
-//axios.defaults.baseURL = 'https://recordsserver.onrender.com';
+//axios.defaults.baseURL = 'https://rabbit-records-back.vercel.app/';
 axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
